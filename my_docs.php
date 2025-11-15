4228406e16f8e2fa9115ad2cd31464d68975879a
c22m02i93 <106078649+c22m02i93@users.noreply.github.com>
2025-11-15 22:12:23 +0400

    Update my_docs.php

--- my_docs.php
+++ my_docs.php
@@ -1,119 +1,100 @@
-<?
-if (isset($_REQUEST[session_name()])) session_start ();
-$auth=$_SESSION['auth'];
-$name_user=$_SESSION['name_user'];
-if ($auth!=1) {Header ("Location: my_auth.php");}
+<?php
+if (isset($_REQUEST[session_name()])) session_start();
+$auth = $_SESSION['auth'];
+$name_user = $_SESSION['name_user'];
+if ($auth != 1) { Header("Location: my_auth.php"); }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<?
-include 'head.php';
-?>
-<title>Закачка документов</title>
-
+<?php include 'head.php'; ?>
+<title>Р—Р°РіСЂСѓР·РєР° РґРѕРєСѓРјРµРЅС‚Р°</title>
 </head>
+
 <body>
-
 <div style="box-shadow: 0 0 20px rgba(0,0,0,0.5);">
-<?
-include 'golova.php';
-
-include 'menu.php';
-
-include 'content.php';
-
-?>
-<div id="osnovnoe">
-
-<h1>Закачка документов</h1>
 
 <?php
- $submit = $_POST['submit'];
+include 'golova.php';
+include 'menu.php';
+include 'content.php';
+?>
+
+<div id="osnovnoe">
+
+<h1>Р—Р°РіСЂСѓР·РєР° РґРѕРєСѓРјРµРЅС‚Р°</h1>
+
+<?php
+$submit = $_POST['submit'];
+
 if ($submit) {
 
- $data = Date("Y.m.d H:i");
+    $data = Date("Y.m.d H:i");
 
-###
+#################################################
 function rus2translit($string) {
     $converter = array(
-        'а' => 'a',   'б' => 'b',   'в' => 'v',
-        'г' => 'g',   'д' => 'd',   'е' => 'e',
-        'ё' => 'e',   'ж' => 'zh',  'з' => 'z',
-        'и' => 'i',   'й' => 'y',   'к' => 'k',
-        'л' => 'l',   'м' => 'm',   'н' => 'n',
-        'о' => 'o',   'п' => 'p',   'р' => 'r',
-        'с' => 's',   'т' => 't',   'у' => 'u',
-        'ф' => 'f',   'х' => 'h',   'ц' => 'c',
-        'ч' => 'ch',  'ш' => 'sh',  'щ' => 'sch',
-        'ь' => '',    'ы' => 'y',   'ъ' => '',
-        'э' => 'e',   'ю' => 'yu',  'я' => 'ya',
-        
-        'А' => 'A',   'Б' => 'B',   'В' => 'V',
-        'Г' => 'G',   'Д' => 'D',   'Е' => 'E',
-        'Ё' => 'E',   'Ж' => 'Zh',  'З' => 'Z',
-        'И' => 'I',   'Й' => 'Y',   'К' => 'K',
-        'Л' => 'L',   'М' => 'M',   'Н' => 'N',
-        'О' => 'O',   'П' => 'P',   'Р' => 'R',
-        'С' => 'S',   'Т' => 'T',   'У' => 'U',
-        'Ф' => 'F',   'Х' => 'H',   'Ц' => 'C',
-        'Ч' => 'Ch',  'Ш' => 'Sh',  'Щ' => 'Sch',
-        'Ь' => '',    'Ы' => 'Y',   'Ъ' => '',
-        'Э' => 'E',   'Ю' => 'Yu',  'Я' => 'Ya',
-   		'№' => 'No',
+        'Р°'=>'a','Р±'=>'b','РІ'=>'v','Рі'=>'g','Рґ'=>'d','Рµ'=>'e','С‘'=>'e','Р¶'=>'zh','Р·'=>'z',
+        'Рё'=>'i','Р№'=>'y','Рє'=>'k','Р»'=>'l','Рј'=>'m','РЅ'=>'n','Рѕ'=>'o','Рї'=>'p','СЂ'=>'r',
+        'СЃ'=>'s','С‚'=>'t','Сѓ'=>'u','С„'=>'f','С…'=>'h','С†'=>'c','С‡'=>'ch','С€'=>'sh','С‰'=>'sch',
+        'СЉ'=>'','С‹'=>'y','СЊ'=>'','СЌ'=>'e','СЋ'=>'yu','СЏ'=>'ya',
+
+        'Рђ'=>'A','Р‘'=>'B','Р’'=>'V','Р“'=>'G','Р”'=>'D','Р•'=>'E','РЃ'=>'E','Р–'=>'Zh','Р—'=>'Z',
+        'Р'=>'I','Р™'=>'Y','Рљ'=>'K','Р›'=>'L','Рњ'=>'M','Рќ'=>'N','Рћ'=>'O','Рџ'=>'P','Р '=>'R',
+        'РЎ'=>'S','Рў'=>'T','РЈ'=>'U','Р¤'=>'F','РҐ'=>'H','Р¦'=>'C','Р§'=>'Ch','РЁ'=>'Sh','Р©'=>'Sch',
+        'РЄ'=>'','Р«'=>'Y','Р¬'=>'','Р­'=>'E','Р®'=>'Yu','РЇ'=>'Ya'
     );
     return strtr($string, $converter);
 }
+
 function str2url($str) {
-    // переводим в транслит
     $str = rus2translit($str);
-    // в нижний регистр
     $str = strtolower($str);
-    // заменям все ненужное нам на "-"
     $str = preg_replace('~[^-a-z0-9\._]+~u', '_', $str);
-    // удаляем начальные и конечные '-'
     $str = trim($str, "-");
     return $str;
 }
+#################################################
 
-############################################
-
-$name_doc = str2url($_FILES['uploadfile']['name']);
+$name_doc  = str2url($_FILES['uploadfile']['name']);
 $name_time = str2url($data);
 
-$uploadfile = 'TYPE/'.$name_time.'_'.$name_doc;
+$uploadfile = 'TYPE/' . $name_time . '_' . $name_doc;
 
 copy($_FILES['uploadfile']['tmp_name'], $uploadfile);
-	
-$size=$_FILES['uploadfile']['size']/1024;
- $size=round($size, 1);
-	
-echo '<p style="color:#135B00; text-align: center">Документ <b>http://barysh-eparhia.ru/'.$uploadfile.'</b> успешно добавлен!</p><br />';
 
+$size = $_FILES['uploadfile']['size'] / 1024;
+$size = round($size, 1);
 
+echo '<p style="color:#135B00; text-align:center">
+Р”РѕРєСѓРјРµРЅС‚ <b>http://barysh-eparhia.ru/'.$uploadfile.'</b> СѓСЃРїРµС€РЅРѕ Р·Р°РіСЂСѓР¶РµРЅ!
+</p><br />';
 }
-
 ?>
-	<TABLE CELLSPACING=3 CELLPADDING=2 width='400' align='center' border=0>
-        <FORM ACTION='my_docs.php' method='post' enctype=multipart/form-data>
-		<TR><TD VALIGN=top> Выберите файл с компьютера
-		</TD><TD> <input type=file name="uploadfile" required></TD></TR>
 
+<table cellspacing="3" cellpadding="2" width="400" align="center" border="0">
+<form action="my_docs.php" method="post" enctype="multipart/form-data">
 
+<tr>
+    <td valign="top">Р’С‹Р±РµСЂРёС‚Рµ С„Р°Р№Р» РґР»СЏ Р·Р°РіСЂСѓР·РєРё:</td>
+    <td><input type="file" name="uploadfile" required></td>
+</tr>
 
-		<TR><TD VALIGN=top colspan=2>
-	<INPUT TYPE='submit' name='submit' value='Добавить' />
-        <INPUT TYPE='reset' value='Очистить'></TD></TR>
- </FORM>  
+<tr>
+    <td colspan="2" valign="top">
+        <input type="submit" name="submit" value="Р—Р°РіСЂСѓР·РёС‚СЊ" />
+        <input type="reset" value="РћС‡РёСЃС‚РёС‚СЊ" />
+    </td>
+</tr>
 
-	</TABLE>	
+</form>
+</table>
 
 </div>
 
-<?
-include 'footer.php';
-?>
+<?php include 'footer.php'; ?>
 
- </div>
+</div>
 </body>
 </html>
